e5df4301a99c5b14a12cd5e1a513e07a1f8fd2a7
August Feng <au.fengster@gmail.com>
2022-07-26 17:15:56 -0400

    add sops-encrypt function

--- sops-mode.el
+++ sops-mode.el
@@ -36,12 +36,30 @@
         (compilation-mode nil))
       (display-buffer output '(nil (allow-no-window . t))))))
 
+(defun sops-encrypt ()
+  "encrypt file using sops."
+  (interactive)
+  (save-some-buffers (not compilation-ask-about-save))
+  (let ((cmd (car sops-command))
+        (args (list "-e" (file-truename (buffer-file-name))))
+        (output (get-buffer-create "*encrypted*")))
+    (with-current-buffer output
+      (setq buffer-read-only nil)
+      (erase-buffer)
+      (if (zerop (apply #'call-process cmd nil t nil args))
+          (progn
+            (json-mode)
+            (view-mode))
+        (compilation-mode nil))
+      (display-buffer output '(nil (allow-no-window . t))))))
+
 ;;;###autoload
 (define-minor-mode sops-mode
   "encrypt/decrypt files with sops."
   :lighter " sops"
   :keymap (let ((map (make-sparse-keymap)))
             (define-key map  (kbd "C-c C-d") 'sops-decrypt)
+            (define-key map  (kbd "C-c C-c") 'sops-encrypt)
             map))
 
 ;;;###autoload
